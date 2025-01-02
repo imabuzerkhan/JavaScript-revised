0ded8a277a88ecde824757ae273cee56e86f63c7
imabuzerkhan <abuzerk41@gmail.com>
2025-01-02 16:44:59 +0545

    Data types String

---
+++ Day 1/Datatypes/String.js
@@ -0,0 +1,13 @@
+// in javascript all the charachter are strings . chaye wo single characther oh ya bhaut sara
+
+// * example of single chracter string
+var singleChar = "c";
+console.log(typeof(singleChar)) //* checking thetype of datatypes
+console.log(singleChar)
+
+// * exampleof multicharacter string
+
+let multicharactrer = 'abuzer you are fool'
+console.log(typeof(multicharactrer)) //* checking thetype of datatypes
+
+console.log(multicharactrer)
