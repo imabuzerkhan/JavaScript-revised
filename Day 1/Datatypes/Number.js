bab834937df4eca4178990751d89d214443dfe06
imabuzerkhan <abuzerk41@gmail.com>
2025-01-02 16:39:49 +0545

    datatypes

---
+++ Day 1/Datatypes/Number.js
@@ -0,0 +1,8 @@
+// Number is primitive data types in javascript. in js there is no different datatypes for float . there is only data types for both integer and javascript
+
+var number = 23 ; //* example of integer number
+console.log(number)
+
+// * example of float number
+let Number = 23.5
+console.log(Number)
