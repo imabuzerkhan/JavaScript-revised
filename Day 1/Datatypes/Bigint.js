4c92acf5c7a812fc6526510e8e7813886e209e0b
imabuzerkhan <abuzerk41@gmail.com>
2025-01-02 17:15:52 +0545

    bigint

---
+++ Day 1/Datatypes/Bigint.js
@@ -0,0 +1,7 @@
+//  bigint bade value ko stroe karwate hai jaise kii 2 ka power 53 ke value upar ka jo value hoga usse store karega
+
+let number = 1234567891010111213141516171819202121212324252627282930313233343536n //* it is a bigint number  we write and in the end 
+console.log(number)
+
+let num = BigInt("1234567891010111213141516171819202121212324252627282930313233343536")
+console.log(num)
