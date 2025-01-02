99503d939dc9f81de14b6386393f417d91469424
imabuzerkhan <abuzerk41@gmail.com>
2025-01-02 16:56:03 +0545

    String type

--- Day 1/Datatypes/String.js
+++ Day 1/Datatypes/String.js
@@ -11,3 +11,26 @@ let multicharactrer = 'abuzer you are fool'
 console.log(typeof(multicharactrer)) //* checking thetype of datatypes
 
 console.log(multicharactrer)
+
+
+// * templates literal string
+
+let marks = 98;
+let English = `The total marks of english is ${marks}`
+console.log(English)
+
+// * it is possibleto add numbers and sting
+
+var num = 23;
+console.log("the type of num is", typeof(num));
+var letter = 'shayam';
+var add = num + letter ;
+console.log(add)
+console.log("the type of add is", typeof(add));
+
+// * length function in string
+var Myname = `Mohammad 
+abuzer  
+khan`
+var checking = Myname.length;
+console.log(checking)
