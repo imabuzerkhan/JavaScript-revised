f330700a3643adbd9c80bcde2274aca2f24a9ffd
imabuzerkhan <abuzerk41@gmail.com>
2025-01-02 17:07:08 +0545

    null

--- Day 1/Datatypes/Null.js
+++ Day 1/Datatypes/Null.js
@@ -2,3 +2,12 @@
 
 let value = null ; // iska matlab hota hai khali agar koi value store nhi karna hai to null date  hai usme 
 console.log(value)
+
+
+// * example using if else
+const var1 = null;
+if (var1) {
+    console.log('var1 is not null');
+} else {
+    console.log('var1 is null');
+}
