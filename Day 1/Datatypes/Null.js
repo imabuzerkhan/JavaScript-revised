4a8019ada894b8d194f297a457ecbacf661148ce
imabuzerkhan <abuzerk41@gmail.com>
2025-01-02 17:06:02 +0545

    nuull in js

---
+++ Day 1/Datatypes/Null.js
@@ -0,0 +1,4 @@
+//  null in javascript 
+
+let value = null ; // iska matlab hota hai khali agar koi value store nhi karna hai to null date  hai usme 
+console.log(value)
---
+++ Day 1/Datatypes/Undefined.js
@@ -0,0 +1,6 @@
+// undefined in javascript
+
+// * example of undefined
+var myname ;
+console.log(myname)
+// * it return undefined the variable is not store anything or its empty
