8b85940dfaa5c908d9a57c87dd68d96d71c674b6
imabuzerkhan <abuzerk41@gmail.com>
2025-01-03 21:16:09 +0545

    Not operator

---
+++ Day 2/Operators/Logical.js
@@ -0,0 +1,15 @@
+//  logical operator
+
+// ! Start from Not operator
+// * for example it denotes with (&)
+// ? if both value are true then it will return true oterwise it return false
+
+const a = 23;
+const b = 10 ;
+const c = 12 ;
+
+if(a == b && b > c){
+  console.log("a is the grater number")
+}else{
+  console.log("Both value are not true that why its return false")
+}
