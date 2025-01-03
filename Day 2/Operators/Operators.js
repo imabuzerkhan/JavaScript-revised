1f6b22a6a133921e68e023d77a2410a912104aaa
imabuzerkhan <abuzerk41@gmail.com>
2025-01-03 21:10:09 +0545

    done for assignment operator

---
+++ Day 2/Operators/Operators.js
@@ -0,0 +1,9 @@
+//  There are several operators in javascript 
+// * they are
+// ! 1. Assignment Operator
+// ! 2 . Arthimethic Operator
+// ! 3 . Comparison Operator/Relation Operator
+// ! 4 . Logical Operator
+// ! 5 . Bitwise Operator
+
+
