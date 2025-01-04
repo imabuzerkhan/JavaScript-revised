0ca7df55b32ab0373832729f4c7b330e0c0ebd00
imabuzerkhan <abuzerk41@gmail.com>
2025-01-04 08:58:00 +0545

    Assignment operator

---
+++ Day 2/Operators/Comparision.js
@@ -0,0 +1,18 @@
+//  comparison operator
+
+// ! == <= >= < >
+
+// ? for example
+
+let a = 23;
+let b = 23;
+//  if(a==b){
+// if(a<b){
+  // if(a>b){
+    // if(a<=b){
+      if(a>=b){
+  console.log(true)
+ }
+ else{
+  console.log(false)
+ }
