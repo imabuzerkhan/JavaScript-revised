4e5249ab47c997edf904f55c9e40e3e59195345a
imabuzerkhan <abuzerk41@gmail.com>
2025-01-08 17:02:42 +0545

    gradient project

--- Project/1. gradient project/Gradient.js
+++ Project/1. gradient project/Gradient.js
@@ -1,5 +1,8 @@
 let Button1 = document.getElementById("button1");
 let Button2 = document.getElementById("button2");
+let copycolour = document.querySelector(".copycode")
+let rgb1  ;
+let rgb2 ;
 
 const hexValues = () => {
   const Hexcolour = "0123456789abcdef";
@@ -12,13 +15,24 @@ const hexValues = () => {
     randomColor += Hexcolour[randomIndex];
   }
 
-  // console.log(randomColor);
+ return randomColor;
 };
 
 let HandleButton1 = ()=>{
-  let rgb1 = hexValues();
-  console.log(rgb1)
+  rgb1 = hexValues();
+  console.log(rgb1);
+  document.body.style.backgroundImage = `linear-gradient(${rgb1} , ${rgb2})`; 
+  Button1.innerText = rgb1;
+  copycolour.innerHTML = ` backgroundImage = linear-gradient(${rgb1} , ${rgb2})`
+}
+let HandleButton2 = ()=>{
+ rgb2 = hexValues();
+  console.log(rgb2);
+  document.body.style.backgroundImage = `linear-gradient(${rgb1}, ${rgb2})`; 
+  Button2.innerText = rgb2; 
+   copycolour.innerHTML = ` backgroundImage = linear-gradient(${rgb1} , ${rgb2})`
 }
 
 // Add event listener for Button1
 Button1.addEventListener("click", HandleButton1);
+Button2.addEventListener("click", HandleButton2);
