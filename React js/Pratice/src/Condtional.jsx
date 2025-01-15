fda9ab93b53456239c839d4539d57049a70a9441
imabuzerkhan <abuzerk41@gmail.com>
2025-01-15 12:31:03 +0100

    another way

--- React js/Pratice/src/Condtional.jsx
+++ React js/Pratice/src/Condtional.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 
 const Condtional = () => {
   let age = 16
+  let canWatch = "Not vote"
+  if(age>= 18) canWatch = " Vote now"
   return (
     <div>
       {/* write conditional statment */}
       <h1>You are trying to vote now</h1>
-      <h2>you can {age>=18 ? "votenow" : "Not vote"} </h2>
+      {/* <h2>you can {age>=18 ? "votenow" : "Not vote"} </h2> */}
+      {/* Another way */}
+      <h2>{canWatch}</h2>
       
       
     </div>
