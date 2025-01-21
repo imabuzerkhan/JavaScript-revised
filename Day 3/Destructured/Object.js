91b0040e2c47e50235e0455fc3026a4aed3bf72c
imabuzerkhan <abuzerk41@gmail.com>
2025-01-21 19:24:51 +0545

    play with object

---
+++ Day 3/Destructured/Object.js
@@ -0,0 +1,10 @@
+// destructured with object 
+const Student = {
+  name : "abuzer",
+  age : 25 ,
+  Address : "biratnagar-06"
+}
+
+console.log(Student.name);
+console.log(Student.age)
+console.log(Student.Address);
