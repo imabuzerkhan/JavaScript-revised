aa09a5f0c19390d72cb18df702b9391ab462db22
imabuzerkhan <abuzerk41@gmail.com>
2025-01-23 16:10:07 +0545

    UNDERSTANDING SPREAD

--- Day 3/Destructured/Spread.js
+++ Day 3/Destructured/Spread.js
@@ -1,4 +1,15 @@
-function Spread (username , ...number){
-    console.log(username , number)
-}
-Spread("abuzer" , 40, 10 ,50)
+// function Spread (username , ...number){
+//     console.log(username , number)
+// }
+// Spread("abuzer" , 40, 10 ,50)
+
+// ! spread operator
+let a = [1,2,3,4,5,6,7,8,9,10]
+let b = [...a , 11]
+console.log(a)
+console.log("----------------------------------")
+console.log(b)
+
+// speard 
+let value = [1,2,3,4,6]
+console.log(Math.min(...value))
